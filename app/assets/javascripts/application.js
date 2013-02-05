088194fa8cfc5699967ac634f82963485f00518c
alexoviedo999 <alexoviedo999@gmail.com>
2013-02-04 22:24:01 -0500

    Added Raphael and Morris gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require 'raphael-min.js'
+//= require 'morris.min.js'
 //= require bootstrap
+//= require 'stocks_info_app.js'
 //= require_tree .
